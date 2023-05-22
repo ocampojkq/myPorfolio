205d99ec87e611b7d38ba4c7af6489018b9cc50a
ocampojkq <ocampo.jessekit1@gmail.com>
2023-05-23 07:03:19 +0800

    adding rain drops

--- assets/js/main.js
+++ assets/js/main.js
@@ -271,3 +271,34 @@
    */
   new PureCounter();
 })();
+
+// raind drops
+
+const createRaindrop = () => {
+  const raindrop = document.createElement("div");
+  raindrop.classList.add("raindrop");
+  raindrop.style.left = Math.random() * window.innerWidth + "px";
+  raindrop.style.backgroundColor = getRandomColor();
+  document.body.appendChild(raindrop);
+  startRaindropAnimation(raindrop);
+};
+
+const startRaindropAnimation = (raindrop) => {
+  const animationDuration = Math.random() * 4 + 2; // Random duration between 2 and 6 seconds
+  raindrop.style.animation = `raindropFall ${animationDuration}s linear infinite`;
+};
+
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+// Generate raindrop elements
+const numRaindrops = 100;
+for (let i = 0; i < numRaindrops; i++) {
+  createRaindrop();
+}
